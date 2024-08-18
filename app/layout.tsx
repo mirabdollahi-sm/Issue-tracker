a2d758b9cd8bd1821c59c9152539a903869a81ba
محمد میرعبدالهی <mohamad.mirabdollahi@samim.net>
2024-08-18 13:03:32 +0330

    Style new issue page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
-import "./globals.css";
 import "@radix-ui/themes/styles.css";
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
 import Navbar from "./Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,9 +22,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+    <html lang="en" >
+      <body className={inter.variable}>
+        <Theme accentColor="violet">
           <Navbar />
           <main className="p-5">{children}</main>
         </Theme>
