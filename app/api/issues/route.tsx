b011c757bf2834d25817702e9e103e2c8587e144
محمد میرعبدالهی <mohamad.mirabdollahi@samim.net>
2024-08-20 17:42:49 +0330

    Show the issues.

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
 import prisma from "@/prisma/client";
 import { createIssueSchema } from '../../validationSchemas'
 
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,17 +1,44 @@
-"use client"
+/* eslint-disable @next/next/no-async-client-component */
+"use client";
 
 import React from "react";
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
+import prisma from "@/prisma/client";
 
-const issues = () => {
+const IssuesPage = async () => {
+  const issuses = await prisma.issue.findMany();
   return (
     <div>
-      <Button>
-        <Link href="/issues/new">New Issue</Link>
-      </Button>
+      <div className="mb-5">
+        <Button>
+          <Link href="/issues/new">New Issue</Link>
+        </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">Created At</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        {issuses.map((issue) => {
+          return (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden">Open</div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">Open</Table.Cell>
+              <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
+            </Table.Row>
+          );
+        })}
+        <Table.Body></Table.Body>
+      </Table.Root>
     </div>
   );
 };
 
-export default issues;
+export default IssuesPage;
